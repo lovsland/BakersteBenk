9ca666a20435c45eccf6f785908b8f816d02f3fb
Martin Liljenstrøm <35597881+Bartin95@users.noreply.github.com>
2018-02-14 15:38:49 +0100

    Rettelse

--- Oppgave 4/4C/ascii/iso_test.go
+++ Oppgave 4/4C/ascii/iso_test.go
@@ -2,7 +2,6 @@ package ascii
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestExtendedASCIIText(t *testing.T) {
@@ -16,9 +15,7 @@ func isExtendedASCII(a2 string) bool {
 	for _, i := range a2 {
 		if i < 0x80 || i > 0xff {
 			return false
-			fmt.Print("Inneholder andre tegn")
 		}
 	}
-	fmt.Print("Inneholder bare extended ASCII")
 	return true
 }
